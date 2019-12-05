47d5ecae4aab95c9788c15c5ada82a0f3e53b7c2
Kadyr Poyraz <poyraz_2001@mail.ru>
2019-12-05 18:53:27 +0200

    Поменял стили формы

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -3,10 +3,10 @@ import {Button, FormGroup, Input} from "reactstrap";
 
 const TextForm = props => {
     return (
-        <FormGroup className="w-25 d-flex">
-            <Input onChange={props.changeAuthor} id="author" placeholder="Author" value={props.inpAuthor}/>
+        <FormGroup className="w-50 d-flex m-3">
+            <Input className='mr-3 w-25' onChange={props.changeAuthor} id="author" placeholder="Author" value={props.inpAuthor}/>
             <Input onChange={props.changeMessage} id="message" placeholder="Text" value={props.inpMessage}/>
-            <Button>Enter</Button>
+            <Button className='ml-3'>Add</Button>
         </FormGroup>
     );
 };
