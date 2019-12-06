547e9ccf004bd80fc4b14c39ed0065d87bc3aef9
Kadyr Poyraz <poyraz_2001@mail.ru>
2019-12-06 19:12:14 +0200

    Сделал выделения нкнеймов пользователей и даты жирным шрифтом

--- src/components/listMessage/listMessage.js
+++ src/components/listMessage/listMessage.js
@@ -9,9 +9,9 @@ const ListMessage = (
     const messageDate = +dateFormat(date, 'd') !== now ? dateFormat(date, 'd.m.yy h:s') : dateFormat(date, 'h:MM TT');
     return (
         <ListGroupItem className='d-flex align-items-center'>
-            <span className="pr-1 mr-2 border-dark border rounded bg-light p-1">{author}</span>
+            <span className="font-weight-bold pr-1 mr-2 mb-auto border-dark border rounded bg-light p-1">{author}</span>
             <span>{text}</span>
-            <span className='ml-auto text-muted' style={{fontSize: '10px'}}>{messageDate}</span>
+            <span className='ml-auto text-muted font-weight-bold' style={{fontSize: '10px'}}>{messageDate}</span>
         </ListGroupItem>
     );
 };
